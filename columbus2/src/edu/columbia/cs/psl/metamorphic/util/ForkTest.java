6c10db7a5a7af178be41c3861c4c5e1f600c7518
Jonathan Bell <jbell@cs.columbia.edu>
2012-03-01 00:18:35 -0500

    Added forker and some socket stuff in a tester. NB we really need to be forking inside of the interceptor, i think, instead of making new threads so that we get a real clone of the state, and not a BS clone from calling the clone method, which may or may not be properly implemented.

---
+++ columbus2/src/edu/columbia/cs/psl/metamorphic/util/ForkTest.java
@@ -0,0 +1,213 @@
+package edu.columbia.cs.psl.metamorphic.util;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+
+public class ForkTest extends SimpleChannelHandler
+{
+	
+	  @Override
+	    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+		  ChannelBuffer buf = (ChannelBuffer) e.getMessage();
+		    while(buf.readable()) {
+		        System.out.println((char) buf.readByte());
+		        System.out.flush();
+		    }	    }
+
+	    @Override
+	    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+	        e.getCause().printStackTrace();
+	        
+	        Channel ch = e.getChannel();
+	        ch.close();
+	    }
+	    
+    public static void main(String[] args)
+    {
+	ForkTest test = new ForkTest();
+	//test.go();
+	try{
+	test.loop();
+	}
+	catch(Exception ex)
+	{
+		ex.printStackTrace();
+	}
+    }
+    private void makeServer()
+    {
+    	final ForkTest parent = this;
+    	  ChannelFactory factory =
+    	            new NioServerSocketChannelFactory(
+    	                    Executors.newCachedThreadPool(),
+    	                    Executors.newCachedThreadPool());
+
+    	        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+    	        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+					
+					@Override
+					public ChannelPipeline getPipeline() throws Exception {
+						// TODO Auto-generated method stub
+    	                return org.jboss.netty.channel.Channels.pipeline(parent);
+					}
+				});
+
+    	        bootstrap.setOption("child.tcpNoDelay", true);
+    	        bootstrap.setOption("child.keepAlive", true);
+
+    	        bootstrap.bind(new InetSocketAddress(4356));
+    }
+    private int flag = 0;
+    public void loop() throws Exception
+    {
+	    System.out.println("parent "  + flag);
+	    makeServer();
+	    int pid = Forker.fork();
+
+	    if (pid == 0)
+	    {
+	    	flag = 3;
+		System.out.println("child " + flag);
+		SocketChannel channel = null;
+    	try{
+    	 channel = SocketChannel.open();
+    	 System.out.println("Connecting");
+    	channel.connect(new InetSocketAddress("localhost", 4356));
+    	System.out.println("Connected");
+    	}
+    	catch(Exception ex)
+    	{
+    	ex.printStackTrace();	
+    	}
+
+		flag = 10;
+		try {
+//			Thread.currentThread().sleep(100);
+			String newData = "New String to write to file..." + System.currentTimeMillis();
+
+			ByteBuffer buf = ByteBuffer.allocate(48);
+			buf.clear();
+			buf.put(newData.getBytes());
+
+			buf.flip();
+
+			while(buf.hasRemaining()) {
+			    channel.write(buf);
+			}
+			channel.close();
+			System.out.println("Closed channel");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Forker.exit();
+	    }
+	    else
+	    {
+	    	try{
+//	    	SocketChannel channel = SocketChannel.open();
+//	    	channel.connect(new InetSocketAddress("localhost", 4356));
+	    	}
+	    	catch(Exception ex)
+	    	{
+	    		ex.printStackTrace();
+	    	}
+	    	System.out.println("Parent pid: " + pid);
+	    	flag = 1;
+	    	System.out.println("Set flag");
+//	    	try {
+//				Thread.currentThread().sleep(0);
+//			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+	    }
+	
+    }
+
+    public void go()
+    {
+	try
+	{
+	    Forker forker = new Forker();
+
+	    // do the fork
+	    int pid = forker.fork();
+
+	    //System.out.println(pid);
+	    if (pid == 0)
+	    {
+		try
+		{
+		    // this is the child
+		    System.out.println("CHILD");
+
+		    PrintWriter out = new PrintWriter(new File("foo.txt"));
+		    System.out.println("Created PrintWriter");
+
+		    out.println("Hello world!!!");
+		    out.flush();
+		    System.out.println("done writing");
+
+/*
+		    Socket connect = new Socket("localhost", 1234);
+		    System.out.println("created a socket");
+
+		    // get the input stream
+		    Scanner in = new Scanner(connect.getInputStream());	    
+		    // get the output stream
+		    PrintWriter out = new PrintWriter(connect.getOutputStream());
+
+		    out.write("5\n");
+		    out.flush();
+		    System.out.println("wrote");
+
+		    System.out.println(in.nextLong());
+
+		    out.close();
+		    */
+
+		}
+		finally
+		{
+		    System.out.println("About to call EXIT");
+		    forker.exit();
+		    System.exit(0);
+		    System.out.println("Still... alive");
+		}
+	    }
+	    else
+	    {
+		System.out.println("PARENT");
+	    }
+
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+	
+	try { Thread.sleep(1000); } catch (Exception e) { }
+
+    }
+}
